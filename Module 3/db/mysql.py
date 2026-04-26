eb6f5cb55fd83ef1c0aa4cc3e93584d434de58e6
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 17:40:38 +0700

    Add MySQL database connection setup

--- Module 3/db/mysql.py
+++ Module 3/db/mysql.py
@@ -1,1 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
+MYSQL_URL = os.getenv("MYSQL_URL")
+
+engine = create_engine(MYSQL_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_mysql_db():
+    return SessionLocal()
