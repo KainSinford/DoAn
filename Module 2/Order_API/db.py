1c6c08ec76b91be2b439cb0a00de586c0b542c86
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 16:17:11 +0700

    Update db.py

--- Module 2/Order_API/db.py
+++ Module 2/Order_API/db.py
@@ -1,1 +1,27 @@
+import mysql.connector
 
+def get_connection():
+    return mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password="root",
+        database="orders"
+    )
+
+def insert_order(user_id, product_id, quantity):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO orders (user_id, product_id, quantity, status)
+    VALUES (%s, %s, %s, 'PENDING')
+    """
+    cursor.execute(query, (user_id, product_id, quantity))
+    conn.commit()
+
+    order_id = cursor.lastrowid
+
+    cursor.close()
+    conn.close()
+
+    return order_id
