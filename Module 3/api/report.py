1783c1de4e13e8d0f8a1d53c7dedba3ed86061db
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 17:38:30 +0700

    Update report.py

--- Module 3/api/report.py
+++ Module 3/api/report.py
@@ -1,1 +1,21 @@
+from fastapi import APIRouter
+from app.db.mysql import get_mysql_db
+from app.db.postgres import get_postgres_db
+from app.services.report_service import generate_report
 
+router = APIRouter()
+
+@router.get("/api/report")
+def get_report():
+    mysql_db = get_mysql_db()
+    postgres_db = get_postgres_db()
+
+    try:
+        data = generate_report(mysql_db.bind, postgres_db.bind)
+        return {
+            "status": "success",
+            "data": data
+        }
+    finally:
+        mysql_db.close()
+        postgres_db.close()
