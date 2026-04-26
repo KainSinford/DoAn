8a08c4333532163e5530b1e63d3a398924899c11
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 17:41:17 +0700

    Update postgres.py

--- Module 3/db/postgres.py
+++ Module 3/db/postgres.py
@@ -1,1 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
+POSTGRES_URL = os.getenv("POSTGRES_URL")
+
+engine = create_engine(POSTGRES_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_postgres_db():
+    return SessionLocal()
