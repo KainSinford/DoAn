102fc329765a1e17f756236cf3fc1d85268e7be9
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 17:44:46 +0700

    Update config.py

--- Module 3/utils/config.py
+++ Module 3/utils/config.py
@@ -1,1 +1,19 @@
+from pydantic import BaseSettings
 
+
+class Settings(BaseSettings):
+    # MySQL
+    MYSQL_URL: str = "mysql+pymysql://root:password@mysql:3306/webstore"
+
+    # PostgreSQL
+    POSTGRES_URL: str = "postgresql://postgres:password@postgres:5432/finance"
+
+    # App
+    APP_NAME: str = "Dashboard Service"
+    DEBUG: bool = True
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
