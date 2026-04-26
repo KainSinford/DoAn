9986b72194f7783a01250de98a7716376f002922
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 16:19:52 +0700

    Update db_mysql.py

--- Module 2/Order_Worker/db_mysql.py
+++ Module 2/Order_Worker/db_mysql.py
@@ -1,1 +1,20 @@
+import mysql.connector
 
+def get_connection():
+    return mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password="root",
+        database="orders"
+    )
+
+def update_order_status(order_id, status):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = "UPDATE orders SET status=%s WHERE id=%s"
+    cursor.execute(query, (status, order_id))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
