50dfca0be63e020453759e3218a7a9352c28550f
CYKazan <154437931+CYKazan@users.noreply.github.com>
2026-04-26 21:32:02 +0700

    Update main.py

--- Module 2/Order_API/main.py
+++ Module 2/Order_API/main.py
@@ -2,25 +2,37 @@ from flask import Flask, request, jsonify
 from db import insert_order
 from rabbitmq import publish_order
 from models import Order
+import logging
+import os
+
+logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "ok"}
+
 
 @app.route("/api/orders", methods=["POST"])
 def create_order():
     data = request.json
 
     if not data:
-        return jsonify({"error": "Missing JSON body"}), 400
+        return jsonify({"error": "Missing JSON"}), 400
 
-    # Tạo object từ model
     order = Order.from_dict(data)
 
-    # Validate an toàn
-    if not order.user_id or not order.product_id or not order.quantity or order.quantity <= 0:
+    if (
+        order.user_id is None or
+        order.product_id is None or
+        order.quantity is None or
+        order.quantity <= 0
+    ):
         return jsonify({"error": "Invalid data"}), 400
 
     try:
-        # Insert MySQL (PENDING)
         order_id = insert_order(
             order.user_id,
             order.product_id,
@@ -29,19 +41,15 @@ def create_order():
 
         order.order_id = order_id
 
-        # Publish RabbitMQ
         publish_order(order.to_dict())
 
-        return jsonify({
-            "message": "Order received",
-            "order_id": order_id
-        }), 202
+        logger.info(f"Order created: {order_id}")
+
+        return jsonify({"order_id": order_id}), 202
 
     except Exception as e:
-        return jsonify({
-            "error": "Internal server error",
-            "details": str(e)
-        }), 500
+        logger.error(e)
+        return jsonify({"error": "Internal error"}), 500
 
 
 if __name__ == "__main__":
